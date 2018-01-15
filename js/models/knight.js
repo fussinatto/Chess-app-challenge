68bb8c6fe98c6f90fbc3619f2f163545fb544132
Dario Fuzinato <fussinatto@gmail.com>
2018-01-15 20:47:49 +0100

    implement knight movement

--- js/models/knight.js
+++ js/models/knight.js
@@ -18,8 +18,34 @@ var app = app || {};
       app.Pawn.prototype.initialize.apply(this, arguments); // super()
     },
 
+
+    /**
+     * CheckMovement method that verifies validity of the move and eventually sets a new position
+     *
+     * @param square - jQuery element
+     * @param sqareFigure - app.Figure object
+     * @returns {boolean}
+     */
     checkMovement: function(square, sqareFigure) {
-      return app.Pawn.prototype.checkMovement.apply(this, arguments); // super()
+      app.Figure.prototype.checkMovement.apply(this, arguments); // super()
+
+      this.colDifference = Math.abs(this.colDifference);
+      this.rowDifference = Math.abs(this.rowDifference);
+
+      // Allow movement in all directions by one square
+      if ((this.colDifference == 1 && this.rowDifference <= 3) || (this.colDifference == 3 && this.rowDifference <= 1)) {
+        this.set('position', this.newPosition);
+
+        // Eat opponent figure if exists
+        if (sqareFigure) {
+          app.figures.remove(sqareFigure);
+        }
+        return true;
+
+      } else {
+        return false;
+      }
+
     }
 
   });
